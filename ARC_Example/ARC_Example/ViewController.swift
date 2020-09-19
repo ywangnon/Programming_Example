bb913587c9ee6da1e5d4955428d1a1d438f96519
ywangnon <ywangnon123@gmail.com>
2020-09-19 22:48:17 +0900

    ARC 샘플 추가

---
+++ ARC_Example/ARC_Example/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  ARC_Example
+//
+//  Created by Hansub Yoo on 2020/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // 스토리보드의 오토레이아웃값
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet var bottomConstraint: NSLayoutConstraint!
+    
+    // 코드의 오토레이아웃값
+    var codeView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    weak var topConstraintCode: NSLayoutConstraint!
+    var bottomConstraintCode: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 코드 화면 생성
+        self.setCodeViewAutolayout()
+        
+        // 오토레이아웃 비활성화
+        print("\n---------- [ 비활성화 ] ----------\n")
+        self.changeWeakConstraintFromStoryboard()
+        self.changeStrongConstraintFromStoryboard()
+        self.changeWeakConstraintFromCode()
+        self.changeStrongConstraintFromCode()
+        
+        /**
+         오토레이아웃 활성화
+         값이 nil이라면 오류
+         */
+        print("\n---------- [ 활성화 ] ----------\n")
+//        self.changeWeakConstraintFromStoryboard()
+        self.changeStrongConstraintFromStoryboard()
+//        self.changeWeakConstraintFromCode()
+        self.changeStrongConstraintFromCode()
+    }
+    
+    // 코드로 오토레이아웃 생성
+    func setCodeViewAutolayout() {
+        self.view.addSubview(self.codeView)
+        
+        self.topConstraintCode = self.codeView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100)
+        self.bottomConstraintCode = self.codeView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 100)
+        
+        NSLayoutConstraint.activate([
+            self.topConstraintCode,
+            self.bottomConstraintCode,
+            self.codeView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100),
+            self.codeView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 100)
+        ])
+    }
+    
+    // 오토레이아웃 활성화 변화
+    func changeWeakConstraintFromStoryboard() {
+        self.topConstraint.isActive = !self.topConstraint.isActive
+    }
+    
+    func changeStrongConstraintFromStoryboard() {
+        self.bottomConstraint.isActive = !self.bottomConstraint.isActive
+    }
+    
+    func changeWeakConstraintFromCode() {
+        self.topConstraintCode.isActive = !self.topConstraintCode.isActive
+    }
+    
+    func changeStrongConstraintFromCode() {
+        self.bottomConstraintCode.isActive = !self.bottomConstraintCode.isActive
+    }
+    
+}
+
